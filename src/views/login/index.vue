a93352228216adb9df18058d555a4b9492b1c7db
harry <lihaojieharry@gmail.com>
2021-07-20 23:26:36 +0800

    表单验证功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,16 +6,22 @@
     ></el-image>
 
     <!--el-form 表单组件 每个表单项都必须使用el-form-item包裹-->
-    <el-form class="login-form" ref="form" :model="user">
-      <el-form-item>
+
+    <el-form
+      class="login-form"
+      ref="login-form"
+      :model="user"
+      :rules="formRules"
+    >
+      <el-form-item prop="mobile">
         <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="code">
         <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
       </el-form-item>
 
-      <el-form-item>
-        <el-checkbox v-model="checked"
+      <el-form-item prop="agree">
+        <el-checkbox v-model="user.agree"
           >我已阅读并同意用户协议和隐私条款</el-checkbox
         >
       </el-form-item>
@@ -24,7 +30,7 @@
         <el-button
           type="primary"
           :loading="loginLoading"
-          @click="onSubmit"
+          @click="onLogin"
           class="login-btn"
           >立即登录</el-button
         >
@@ -46,25 +52,67 @@ export default {
       user: {
         mobile: "",
         code: "",
+        agree: false, //是否同意协议
       },
-      checked: false, //是否同意选中的状态
+
       img: require("./logo.jpeg"),
       loginLoading: false, //登录的loading状态
+
+      formRules: {
+        //要验证的数据名称：规则列表[]
+        mobile: [
+          { required: true, message: "手机号不能为空", trigger: "change" },
+          {
+            pattern: /^1[3|5|7|8|9]\d{9}$/,
+            message: "请输入正确的手机号",
+            trigger: "change",
+          },
+        ],
+        code: [
+          { required: true, message: "验证码不能为空", trigger: "change" },
+          {
+            pattern: /^\d{6}$/,
+            message: "请输入正确的验证码",
+            trigger: "blur",
+          },
+        ],
+        agree: [
+          {
+            validator: (rule, value, callback) => {
+              //通过 调用callback
+              //shibai callback(new Error('失败'))
+              if (value) {
+                callback();
+              }
+              callback(new Error("请勾选用户协议!!!"));
+            },
+            trigger: "change",
+          },
+        ],
+      },
     };
   },
   methods: {
-    onSubmit() {
+    onLogin() {
       //获取表单数据
-      const user = this.user;
-
+      //const user = this.user;
       //表单验证
-
+      this.$refs["login-form"].validate((valid, err) => {
+        //如果表单验证失败，停止请求
+        if (!valid) {
+          return;
+        }
+        //验证通过，登录成功
+        this.login();
+      });
+    },
+    login() {
       //验证通过
       this.loginLoading = true; //开启登录中loading
       request({
         methods: "POST",
         url: "/mp/v1_0/authorizations",
-        data: user, //data 用来设置post 请求体
+        data: this.user, //data 用来设置post 请求体
       })
         .then((res) => {
           //登录成功
@@ -80,9 +128,8 @@ export default {
           //登录失败
           console.log("登录失败", err);
           this.$message.error("登录失败，手机号或验证码错误！！！");
-          this.loginLoading = false    //关闭loading
+          this.loginLoading = false; //关闭loading
         });
-        
     },
   },
 };
