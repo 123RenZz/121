bcd4eaa385152799222b3392aac5a9a92952ae85
harry <lihaojieharry@gmail.com>
2021-07-20 20:38:10 +0800

    增加页面

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router'
 //在vue cli中创建的项目中 @ 表示 src 目录 的路径 别名
 //好处：它不受当前文件路径影响
 import Login from '@/views/login/index.vue'
+//引入登录后主界面
+import Main from '@/views/main/main.vue'
 
 Vue.use(VueRouter)
 
@@ -12,7 +14,13 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path:'/main',
+    name:'main',
+    component:Main
   }
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/main/main.vue
@@ -0,0 +1,16 @@
+<template>
+   <div id="maincont">  
+     <span> wo shi zhu ye  </span>
+
+   </div>
+</template>
+
+<script>
+export default {
+    
+
+};
+</script>
+
+<style scoped>
+</style>
