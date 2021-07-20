1b5e418eb227cb8b38c3998aad05564505a3586b
harry <lihaojieharry@gmail.com>
2021-07-20 21:28:23 +0800

    登录验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="login-container">
-    <el-image style="width: 100px; height: 100px" :src="require('./logo.jpeg')"></el-image>
+    <el-image
+      style="width: 100px; height: 100px"
+      :src="require('./logo.jpeg')"
+    ></el-image>
 
     <!--el-form 表单组件 每个表单项都必须使用el-form-item包裹-->
     <el-form class="login-form" ref="form" :model="user">
@@ -18,7 +21,11 @@
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="onSubmit" class="login-btn"
+        <el-button
+          type="primary"
+          :loading="loginLoading"
+          @click="onSubmit"
+          class="login-btn"
           >立即登录</el-button
         >
       </el-form-item>
@@ -28,6 +35,8 @@
 
 
 <script>
+import request from "@/utils/request";
+
 export default {
   name: "LoginIndex",
   components: {},
@@ -39,13 +48,41 @@ export default {
         code: "",
       },
       checked: false, //是否同意选中的状态
-      img:require('./logo.jpeg')
-     
+      img: require("./logo.jpeg"),
+      loginLoading: false, //登录的loading状态
     };
   },
   methods: {
     onSubmit() {
-      console.log("submit!");
+      //获取表单数据
+      const user = this.user;
+
+      //表单验证
+
+      //验证通过
+      this.loginLoading = true; //开启登录中loading
+      request({
+        methods: "POST",
+        url: "/mp/v1_0/authorizations",
+        data: user, //data 用来设置post 请求体
+      })
+        .then((res) => {
+          //登录成功
+          console.log(res);
+
+          this.$message({
+            message: "登录成功！",
+            type: "success",
+          });
+          this.loginLoading = false;
+        })
+        .catch((err) => {
+          //登录失败
+          console.log("登录失败", err);
+          this.$message.error("登录失败，手机号或验证码错误！！！");
+          this.loginLoading = false    //关闭loading
+        });
+        
     },
   },
 };
